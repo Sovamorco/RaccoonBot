edd10f0739b0116da77e03f73a18517cb7b39ce4
MrDandycorn <ypsen@yandex.ru>
2019-06-30 18:46:45 +0300

    Сильно поменял проигрывание музыки, добавил кучу команд

--- mainbot.py
+++ mainbot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from random import shuffle
 import time
 import asyncio
 import os
@@ -11,6 +12,9 @@ from music import *
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'), description='Test bot')
 
 queue = []
+current = {}
+queue_exists = False
+queue_msg = ''
 
 
 @bot.event
@@ -77,7 +81,7 @@ async def on_raw_reaction_remove(payload):
 async def update(message):
     async with message.channel.typing():
         check()
-        await message.channel.send('Updated roles!')
+        await message.channel.send('Обновил роли!')
 
 
 @bot.command(name='tts', pass_context=True)
@@ -91,29 +95,23 @@ async def tts_(context, *, text):
     ts = time.time()
     name = 'output{}.mp3'.format(ts)
     create_mp3(text, name)
-    if voice_channel is not None:
-        try:
-            vc = await voice_channel.connect()
-            queue.append((name, discord.FFmpegPCMAudio(name)))
-        except discord.errors.ClientException:
-            queue.append((name, discord.FFmpegPCMAudio(name)))
-            return
-        while queue:
-            name, sound = queue[0]
-            vc.play(sound)
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            if name:
-                os.remove(name)
-            del queue[0]
-        vc.stop()
-        await vc.disconnect()
+    if len(text) < 16:
+        title = text
     else:
-        await text_channel.send('User is not in a channel.')
+        title = '{}...'.format(text[:15])
+    if voice_channel is not None:
+        queue.append({
+            'name': name,
+            'player': discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(name), 5),
+            'title': title
+        })
+        return await play(context, ignore=True)
+    else:
+        await text_channel.send('Пользователь не подключен к каналу')
 
 
 @bot.command(name='why', pass_context=True)
-async def tts_(context, amt):
+async def why_(context, amt):
     global queue
     user = context.message.author
     text_channel = context.message.channel
@@ -123,94 +121,366 @@ async def tts_(context, amt):
     if int(amt) > 20:
         return await text_channel.send('Нет')
     if voice_channel is not None:
-        try:
-            vc = await voice_channel.connect()
-            for i in range(int(amt)):
-                queue.append(('', discord.FFmpegPCMAudio('why.mp3')))
-        except discord.errors.ClientException:
-            for i in range(int(amt)):
-                queue.append(('', discord.FFmpegPCMAudio('why.mp3')))
-            return
-        while queue:
-            name, sound = queue[0]
-            vc.play(sound)
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            if name:
-                os.remove(name)
-            del queue[0]
-        vc.stop()
-        await vc.disconnect()
+        for i in range(int(amt)):
+            queue.append({
+                'name': '',
+                'player': discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('why.mp3'), 5),
+                'title': 'Why'
+            })
+        return await play(context, ignore=True)
     else:
-        await text_channel.send('User is not in a channel.')
+        await text_channel.send('Пользователь не подключен к каналу')
 
 
-@bot.command(name='music', pass_context=True)
-async def music_(context, *, text):
+# @bot.command(name='preload', pass_context=True)
+# async def preload_(context, *, text):
+#     global queue
+#     user = context.message.author
+#     text_channel = context.message.channel
+#     voice_channel = user.voice.channel
+#     if voice_channel is not None:
+#         if not text:
+#             return await play(context)
+#         if ('youtube.com' in text) or ('youtu.be' in text):
+#             searchresult = searchyt(text)[0]
+#             videoID = searchresult['id']
+#             name = '{}.mp3'.format(videoID)
+#             title = searchresult['title']
+#             queue.append({
+#                 'name': name,
+#                 'player': discord.FFmpegPCMAudio(name),
+#                 'title': title
+#             })
+#             return await play(context, ignore=True)
+#         else:
+#             searchresults = searchyt(text)
+#             embed = discord.Embed(title="Choose song")
+#             embedValue = ''
+#             for i in range(len(searchresults)):
+#                 title = searchresults[i]['title']
+#                 embedValue += '{}: {}\n'.format(i + 1, title)
+#             embed.add_field(name='Search results', value=embedValue, inline=False)
+#             await text_channel.send(embed=embed)
+#
+#             def check(m):
+#                 if m.content.isdigit():
+#                     return (0 < int(m.content) < 11) and (m.channel == text_channel)
+#                 return False
+#
+#             msg = await bot.wait_for('message', check=check, timeout=60)
+#             videoID = searchresults[int(msg.content) - 1]['id']
+#             name = '{}.mp3'.format(videoID)
+#             title = searchresults[int(msg.content) - 1]['title']
+#             queue.append({
+#                 'name': name,
+#                 'player': discord.FFmpegPCMAudio(name),
+#                 'title': title
+#             })
+#             return await play(context, ignore=True)
+#         download_video(url)
+#         try:
+#             vc = await voice_channel.connect()
+#             queue.append({
+#                 'name': name,
+#                 'player': discord.FFmpegPCMAudio(name),
+#                 'title': title
+#             })
+#             await text_channel.send('Added {} to the queue'.format(title))
+#         except discord.errors.ClientException:
+#             queue.append({
+#                 'name': name,
+#                 'player': discord.FFmpegPCMAudio(name),
+#                 'title': title
+#             })
+#             await text_channel.send('Added {} to the queue'.format(title))
+#             return
+#         while queue:
+#             name = queue[0].get('name')
+#             sound = queue[0].get('player')
+#             title = queue[0].get('title')
+#             vc.play(sound)
+#             await text_channel.send('Now playing: {}'.format(title))
+#             while vc.is_playing():
+#                 await asyncio.sleep(1)
+#             if name:
+#                 os.remove(name)
+#             del queue[0]
+#         vc.stop()
+#         await vc.disconnect()
+#     else:
+#         await text_channel.send('User is not in a channel.')
+
+
+async def play(context, ignore=False):
+    global queue
+    global current
+    user = context.message.author
+    text_channel = context.message.channel
+    voice_channel = user.voice.channel
+    beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+    genericArgs = "-loglevel 'quiet'"
+    if voice_channel is None:
+        return await text_channel.send('Пользователь не подключен к каналу')
+    if not queue:
+        return await text_channel.send('Нет песен в очереди')
+    try:
+        vc = await voice_channel.connect()
+    except discord.errors.ClientException:
+        vc = context.voice_client
+    if vc.is_playing():
+        if not ignore:
+            return await text_channel.send('Использование ?play <запрос/ссылка>')
+    while queue:
+        current = queue[0]
+        del queue[0]
+        name = current.get('name')
+        sound = current.get('player')
+        title = current.get('title')
+        if sound.__class__ == str:
+            streamUrl = await get_stream_url(sound)
+            sound = discord.FFmpegPCMAudio(streamUrl, before_options=beforeArgs, options=genericArgs)
+        vc.play(sound)
+        await text_channel.send('Сейчас играет: {}'.format(title))
+        while vc.is_playing():
+            await asyncio.sleep(1)
+        if name:
+            os.remove(name)
+        current = {}
+    if queue_exists:
+        await queue_msg.delete()
+    vc.stop()
+
+
+@bot.command(name='stream', aliases=['play'], pass_context=True)
+async def stream_(context, *, text=''):
     global queue
     user = context.message.author
     text_channel = context.message.channel
     voice_channel = user.voice.channel
-    if not text:
-        return await text_channel.send('Использование: ?music <запрос>')
-    searchresults = searchyt(text)
-    embed = discord.Embed(title="Choose song")
-    embedValue = ''
-    for i in range(len(searchresults)):
-        title = searchresults[i]['title']
-        embedValue += '{}: {}\n'.format(i+1, title)
-    embed.add_field(name='Search results', value=embedValue, inline=False)
-    await text_channel.send(embed=embed)
-
-    def check(m):
-        if m.content.isdigit():
-            return (0 < int(m.content) < 11) and (m.channel == text_channel)
-        return False
-
-    msg = await bot.wait_for('message', check=check, timeout=60)
-    videoID = searchresults[int(msg.content)-1]['id']
-    url = searchresults[int(msg.content)-1]['url']
-    name = '{}.mp3'.format(videoID)
-    title = searchresults[int(msg.content)-1]['title']
-    download_video(url)
     if voice_channel is not None:
-        try:
-            vc = await voice_channel.connect()
+        if not text:
+            return await play(context)
+        if 'youtube.com/playlist' in text:
+            searchresults = searchplaylist(text)
+            for video in searchresults:
+                queue.append({
+                    'name': '',
+                    'player': video['url'],
+                    'title': video['title']
+                })
+            await text_channel.send('Добавил в очередь {} песен'.format(len(searchresults)))
+            return await play(context, ignore=True)
+        if ('youtube.com' in text) or ('youtu.be' in text):
+            searchresult = searchyt(text)[0]
             queue.append({
-                'name': name,
-                'player': discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(name), 0.2),
-                'title': title
+                'name': '',
+                'player': searchresult['url'],
+                'title': searchresult['title']
             })
-            await text_channel.send('Added {} to the queue'.format(title))
-        except discord.errors.ClientException:
-            queue.append({
-                'name': name,
-                'player': discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(name), 0.2),
-                'title': title
-            })
-            await text_channel.send('Added {} to the queue'.format(title))
-            return
-        while queue:
-            name = queue[0].get('name')
-            sound = queue[0].get('player')
-            title = queue[0].get('title')
-            vc.play(sound)
-            await text_channel.send('Now playing: {}'.format(title))
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            if name:
-                os.remove(name)
-            del queue[0]
-        vc.stop()
-        await vc.disconnect()
+            await text_channel.send('Добавил в очередь: {}'.format(searchresult['title']))
+            return await play(context, ignore=True)
+        else:
+            searchresults = searchyt(text)
+            embedValue = ''
+            for i in range(len(searchresults)):
+                title = searchresults[i]['title']
+                embedValue += '{}: {}\n'.format(i + 1, title)
+            embed = discord.Embed(title="Выберите песню", description=embedValue)
+            embed.set_footer(text='Автоматическая отмена через 30 секунд')
+            await text_channel.send(embed=embed, delete_after=30)
+
+            def verify(m):
+                if m.content.isdigit():
+                    return (0 < int(m.content) < 11) and (m.channel == text_channel) and (m.author == user)
+                return False
+
+            try:
+                msg = await bot.wait_for('message', check=verify, timeout=30)
+                searchresult = searchresults[int(msg.content) - 1]
+                queue.append({
+                    'name': '',
+                    'player': searchresult['url'],
+                    'title': searchresult['title']
+                })
+                await text_channel.send('Добавил в очередь: {}'.format(searchresult['title']))
+                return await play(context, ignore=True)
+            except Exception as e:
+                return e
     else:
-        await text_channel.send('User is not in a channel.')
+        await text_channel.send('Пользователь не подключен к каналу')
 
 
 @bot.command(name='leave', pass_context=True)
 async def leave_(context):
-    for vc in bot.voice_clients:
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    await vc.disconnect()
+    return await text_channel.send('Отключен от канала')
+
+
+@bot.command(name='join', aliases=['connect'], pass_context=True)
+async def join_(context):
+    user = context.message.author
+    text_channel = context.message.channel
+    voice_channel = user.voice.channel
+    vc = context.voice_client
+    if voice_channel is None:
+        return await text_channel.send('Пользователь не подключен к каналу')
+    if vc is None:
+        await voice_channel.connect()
+        return await text_channel.send('Подключен к каналу {}'.format(voice_channel))
+    if vc.channel == voice_channel:
+        return await text_channel.send('Уже подключен')
+    await vc.move_to(voice_channel)
+    return await text_channel.send('Подключен к каналу {}'.format(voice_channel))
+
+
+@bot.command(name='np', aliases=['current', 'nowplaying'], pass_context=True)
+async def np_(context):
+    global current
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    if not current:
+        return await text_channel.send('Сейчас ничего не играет')
+    return await text_channel.send('Сейчас играет: {}'.format(current['title']))
+
+
+@bot.command(name='queue', aliases=['q', 'playlist'], pass_context=True)
+async def queue_(context):
+    global queue
+    global queue_msg
+    global queue_exists
+    local_queue = queue
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    if not local_queue:
+        return await text_channel.send('Очередь пустая')
+    embedValue = ''
+    length = 10
+    if len(local_queue) < 10:
+        length = len(local_queue)
+    for i in range(length):
+        embedValue += '{}: {}\n'.format(i + 1, local_queue[i]['title'])
+    embed = discord.Embed(title="Очередь", description=embedValue)
+    msg = await text_channel.send(embed=embed)
+    if queue_exists:
+        await queue_msg.delete()
+    queue_exists = True
+    queue_msg = msg
+    if len(local_queue) > 10:
+        page_num = 0
+
+        def verify(reaction, user):
+            return (reaction.message.id == msg.id) and (user != bot.user)
+
+        await msg.add_reaction('▶')
+        await msg.add_reaction('⏭')
+        while True:
+            reaction, user = await bot.wait_for('reaction_add', check=verify)
+            if str(reaction.emoji) == '▶':
+                page_num += 1
+                length = 10
+                if (len(local_queue)-10*page_num) < 10:
+                    length = (len(local_queue)-10*page_num)
+                embedValue = ''
+                for i in range(page_num*10, page_num*10+length):
+                    embedValue += '{}: {}\n'.format(i + 1, local_queue[i]['title'])
+                embed = discord.Embed(title="Очередь", description=embedValue)
+                await msg.edit(embed=embed)
+                await msg.clear_reactions()
+                await msg.add_reaction('⏮')
+                await msg.add_reaction('◀')
+                if page_num != (len(local_queue)//10):
+                    await msg.add_reaction('▶')
+                    await msg.add_reaction('⏭')
+            elif str(reaction.emoji) == '⏭':
+                page_num = len(local_queue) // 10
+                embedValue = ''
+                for i in range(page_num * 10, len(queue)):
+                    embedValue += '{}: {}\n'.format(i + 1, local_queue[i]['title'])
+                embed = discord.Embed(title="Очередь", description=embedValue)
+                await msg.edit(embed=embed)
+                await msg.clear_reactions()
+                await msg.add_reaction('⏮')
+                await msg.add_reaction('◀')
+            elif str(reaction.emoji) == '◀':
+                page_num -= 1
+                length = 10
+                embedValue = ''
+                for i in range(page_num * 10, page_num * 10 + length):
+                    embedValue += '{}: {}\n'.format(i + 1, local_queue[i]['title'])
+                embed = discord.Embed(title="Очередь", description=embedValue)
+                await msg.edit(embed=embed)
+                await msg.clear_reactions()
+                if page_num != 0:
+                    await msg.add_reaction('⏮')
+                    await msg.add_reaction('◀')
+                await msg.add_reaction('▶')
+                await msg.add_reaction('⏭')
+            elif str(reaction.emoji) == '⏮':
+                page_num = 0
+                embedValue = ''
+                for i in range(0, 10):
+                    embedValue += '{}: {}\n'.format(i + 1, local_queue[i]['title'])
+                embed = discord.Embed(title="Очередь", description=embedValue)
+                await msg.edit(embed=embed)
+                await msg.clear_reactions()
+                await msg.add_reaction('▶')
+                await msg.add_reaction('⏭')
+            else:
+                await reaction.remove(user)
+
+
+@bot.command(name='shuffle', pass_context=True)
+async def shuffle_(context):
+    global queue
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    if not queue:
+        return await text_channel.send('Очередь пустая')
+    shuffle(queue)
+    return await text_channel.send('Очередь перемешана')
+
+
+@bot.command(name='stop', pass_context=True)
+async def stop_(context):
+    global queue_exists
+    global current
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    if not current:
+        return await text_channel.send('Сейчас ничего не играет')
+    if vc.is_playing():
+        queue.clear()
+        if queue_exists:
+            queue_exists = False
+            await queue_msg.delete()
+        vc.stop()
         await vc.disconnect()
+
+
+@bot.command(name='skip', pass_context=True)
+async def skip_(context):
+    global current
+    vc = context.voice_client
+    text_channel = context.message.channel
+    if vc is None:
+        return await text_channel.send('Я не подключен к каналу')
+    if not current:
+        return await text_channel.send('Сейчас ничего не играет')
+    if vc.is_playing():
+        vc.stop()
+        return await text_channel.send('{} пропущена')
 
 
 bot.run(discord_bot_token)
--- music.py
+++ music.py
@@ -3,6 +3,7 @@ import youtube_dl
 from credentials import googleServiceKey
 import json
 from html import unescape
+from urllib.parse import urlparse, parse_qs
 
 
 def searchyt(text):
@@ -27,6 +28,42 @@ def searchyt(text):
     return result
 
 
+def searchplaylist(url):
+    result = []
+    parsed = urlparse(url)
+    plid = parse_qs(parsed.query)['list'][0]
+    payload = {
+        'part': 'snippet',
+        'maxResults': '50',
+        'playlistId': plid,
+        'key': googleServiceKey,
+        'pageToken': ''
+    }
+    searchUrl = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    videoUrl = 'https://www.youtube.com/watch?v={}'
+    r = requests.get(searchUrl, params=payload)
+    results = json.loads(r.text)
+    videos = results['items']
+    for video in videos:
+        result.append({
+            'id': video['snippet']['resourceId']['videoId'],
+            'url': videoUrl.format(video['snippet']['resourceId']['videoId']),
+            'title': unescape(video['snippet']['title'])
+        })
+    while 'nextPageToken' in results.keys():
+        payload['pageToken'] = results['nextPageToken']
+        r = requests.get(searchUrl, params=payload)
+        results = json.loads(r.text)
+        videos = results['items']
+        for video in videos:
+            result.append({
+                'id': video['snippet']['resourceId']['videoId'],
+                'url': videoUrl.format(video['snippet']['resourceId']['videoId']),
+                'title': unescape(video['snippet']['title'])
+            })
+    return result
+
+
 def download_video(link):
     ydl_opts = {
         'format': 'worstaudio/worst',
@@ -38,3 +75,13 @@ def download_video(link):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
+        # return ydl.extract_info(link, download=False)
+
+
+async def get_stream_url(link):
+    ydl_opts = {'quiet': True}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        formats = ydl.extract_info(link, download=False)['formats']
+        for ent in formats:
+            if ent['ext'] == 'webm' and ent['format_note'] == 'DASH audio' and ent['acodec'] == 'opus':
+                return ent['url']
